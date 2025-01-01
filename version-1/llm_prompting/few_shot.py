1d4caf40ddff98465c0b64e3a2d0dd25c167a0da
Prasan Yapa <toprasanyapa@gmail.com>
2025-01-01 17:01:20 +0900

    Update few_shot.py
    Modifying zero-shot and few-shot prompting logic for LLM evaluations.

--- version-1/llm_prompting/few_shot.py
+++ version-1/llm_prompting/few_shot.py
@@ -2,12 +2,17 @@ import random
 
 import torch
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
+from openai_utils import length_of_prompt, dispatch_openai_api_requests
+from prompt import PromptKernel
 from training_args import TrainingArguments
 from ..GAT_MTL import GAT_MTL_Model
 from ..MTL_utils import load_data, training_args
 from ..modeling_llms.modeling_llama import LlamaForSequenceClassification
 from ..modeling_llms.modeling_wsw import WSWEmbeddings
+
+_MAX_PROMPT_TOKENS = 25
 
 models = {
     'GAT_MTL': GAT_MTL_Model,
@@ -30,27 +35,62 @@ def main_evaluation():
             for setting in settings:
                 model = model_class().to(device)
                 optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
+                train_loader, val_loader, test_loader = [], [], []
+                llm_eval = False
 
                 if setting == 'full-shot':
                     train_loader, val_loader, test_loader = create_data_loaders(dataset,
                                                                                 setting='full')
                 elif setting == 'few-shot':
+                    llm_eval = True
                     train_loader, val_loader, test_loader = create_data_loaders(dataset,
                                                                                 setting='few',
                                                                                 few_shot_percentage=0.05)
                 elif setting == 'zero-shot':
+                    llm_eval = True
                     test_loader = DataLoader(dataset['test'],
-                                             batch_size=4,
+                                             batch_size=8,
                                              shuffle=False)
 
                 if setting != 'zero-shot':
                     train_model(model, train_loader, optimizer)
 
-                metrics = evaluate_model(model, test_loader)
+                metrics = evaluate_llm(model, val_loader, setting) if llm_eval else evaluate_model(model, test_loader)
                 results[(dataset_name, model_name, setting)] = metrics
                 print(f"{dataset_name} | {model_name} | {setting} - Metrics: {metrics}")
 
     save_results(results)
+
+
+def in_context_prediction(prompt_example, shots, engine, length_test_only=False):
+    showcase_examples = [
+        "{}\nQ: {}\nA: {}\n".format(s["context"], s["utterance"], s["label"]) for s in shots
+    ]
+    input_example = "{}\nQ: {}\nA:".format(prompt_example["context"], prompt_example["utterance"])
+
+    demonstrations = PromptKernel.set_in_context_demonstrations("prompt_tuning",
+                                                                input_example)
+
+    prompt = "\n".join(showcase_examples + [input_example] + demonstrations)
+
+    if length_test_only:
+        prompt_length = length_of_prompt(prompt, _MAX_PROMPT_TOKENS)
+        print("-----------------------------------------")
+        print(prompt_length)
+        print(prompt)
+        return prompt_length
+
+    response = dispatch_openai_api_requests(api_model_name=engine,
+                                            prompt_list=prompt,
+                                            shots=shots,
+                                            max_tokens=_MAX_PROMPT_TOKENS,
+                                            temperature=0.01,
+                                            api_batch=len(prompt))
+
+    prediction = response["choices"][0]
+    prediction["prompt"] = prompt
+    prediction["text"] = prediction["text"][len(prompt):]
+    return prediction
 
 
 def create_data_loaders(dataset, setting='full', few_shot_percentage=1.0):
@@ -76,6 +116,18 @@ def train_model(model, train_loader, optimizer, epochs=10):
             loss = criterion(outputs, data.y.to(device))
             loss.backward()
             optimizer.step()
+
+
+def evaluate_llm(llm, val_loader, setting):
+    llm.eval()
+    predictions = []
+    for x in tqdm(val_loader, total=len(val_loader), desc="Evaluating"):
+        predictions.append(in_context_prediction(prompt_example=x,
+                                                 shots=setting,
+                                                 engine=llm
+                                                 )
+                           )
+    return predictions
 
 
 def evaluate_model(model, test_loader):
